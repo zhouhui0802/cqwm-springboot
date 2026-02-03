89ace8e52730eb0ea824839bad1641cf49fe817f
zhouhui0802 <1741484908@qq.com>
2026-02-03 09:02:47 +0800

    引入百度画图组件

---
+++ sky-take-out/sky-server/src/main/java/com/zh/sky/controller/admin/ReportController.java
@@ -0,0 +1,52 @@
+package com.zh.sky.controller.admin;
+
+
+/**
+ * @author zhouhui
+ * @version 1.0
+ * @description TODO
+ * @date 2026/2/3 8:55
+ */
+
+import com.zh.sky.result.Result;
+import com.zh.sky.service.ReportService;
+import com.zh.sky.vo.TurnoverReportVO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+
+/**
+ * 报表
+ */
+@RestController
+@RequestMapping("/admin/report")
+@Slf4j
+@Api(tags = "统计报表相关接口")
+public class ReportController {
+
+    @Autowired
+    private ReportService reportService;
+
+    /**
+     * 营业额数据统计
+     *
+     * @param begin
+     * @param end
+     * @return
+     */
+    @GetMapping("/turnoverStatistics")
+    @ApiOperation("营业额数据统计")
+    public Result<TurnoverReportVO> turnoverStatistics( @DateTimeFormat(pattern = "yyyy-MM-dd")
+                                                            LocalDate begin,
+                                                        @DateTimeFormat(pattern = "yyyy-MM-dd")
+                                                        LocalDate end) {
+       return Result.success(reportService.getTurnover(begin, end));
+    }
+}
